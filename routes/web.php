46eb51ca34dda6bf3b231b231d9f09ff96fdc717
sam <a0983131803@gmail.com>
2023-11-08 08:41:07 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
 });
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', function () {return view('admin.dashboard');})->name('admin.dashboard');;
+    Route::get('products', [ProductController::class, 'index2'])->name('products.index2');
+
+    Route::controller(ProductController::class)->prefix('products')->group(function () {
+        Route::get('create', 'create')->name('products.create');
+        Route::post('store', 'store')->name('products.store');
+        Route::get('show/{id}', 'show')->name('products.show');
+        Route::get('edit/{id}', 'edit')->name('products.edit');
+        Route::put('edit/{id}', 'update')->name('products.update');
+        Route::delete('destroy/{id}', 'destroy')->name('products.destroy');
+    });
+ 
+});  
+
+
+Route::get('/carts', [CartController::class, 'index'])->name('carts.index');
+Route::post('/add-to-cart/{productId}', [CartController::class, 'addToCart'])->name('carts.add');
+Route::delete('/carts/{id}', [CartController::class, 'removeFromCart'])->name('carts.remove');
+
+
+Route::get('/orders', [OrderController::class,'index'])->name('orders.index');
+Route::get('/orders/create', [OrderController::class,'create'])->name('orders.create');
+Route::post('/orders/store', [OrderController::class, 'store'])->name('orders.store');
+
